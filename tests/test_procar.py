3cfaeba6599fbcfa39280a001d3a623a8765278f
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-08-01 00:46:17 +0100

    Update `test_procar()` (Gamma point duplicated in k-point path so updated parsing reduces nkpts from 48 to 47)

--- tests/test_procar.py
+++ tests/test_procar.py
@@ -17,12 +17,12 @@ def test_procar():
     procar = Procar(datapath / 'PROCAR')
 
     assert procar.nion == 2
-    assert procar.eigenvalues.shape == (1, 48, 20)
-    assert procar.kvecs.shape == (1, 48, 3)
-    assert procar.kweights.shape == (1, 48)
+    assert procar.eigenvalues.shape == (1, 47, 20)
+    assert procar.kvecs.shape == (1, 47, 3)
+    assert procar.kweights.shape == (1, 47)
     assert np.all(procar.kvecs[0][0] == 0.)
-    assert procar.occs.shape == (1, 48, 20)
-    assert procar.get_projection([0], 'all').sum() == 310.418
-    assert procar.get_projection([0], ['s', 'px']).sum() == 59.32300000000001
-    assert procar.get_projection([0], ['s']).sum() + procar.get_projection([0], 'px').sum() == 59.32300000000001
+    assert procar.occs.shape == (1, 47, 20)
+    assert procar.get_projection([0], 'all').sum() == 303.278
+    assert procar.get_projection([0], ['s', 'px']).sum() == 57.95700000000001
+    assert procar.get_projection([0], ['s']).sum() + procar.get_projection([0], 'px').sum() == 57.957
     assert procar.proj_names == ['s', 'py', 'pz', 'px', 'dxy', 'dyz', 'dz2', 'dxz', 'x2-y2']
