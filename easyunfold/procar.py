354c46a6ba75f1f05a25caa1054470c72c19bff5
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2025-03-11 13:35:56 -0600

    Add commented-out debug util code

--- easyunfold/procar.py
+++ easyunfold/procar.py
@@ -69,6 +69,7 @@ class Procar(MSONable):
         # Counter for the number of sections in the PROCAR
         section_counter = 1
         _last_kid = 0
+        # _last_ion = nion  # track ion numbers to ensure all ions being parsed
         this_procar_parsed_kpoints = set()  # set with tuples of parsed (kvec tuple, section_counter) for this PROCAR
         while line:
             if line.startswith(' k-point'):
@@ -95,9 +96,14 @@ class Procar(MSONable):
                             break
                     continue  # go back to start of outer while loop, to parse this k-point
 
-            elif (not re.search(r'[a-zA-Z]', line) and line.strip() and len(line.strip().split()) - 2 == len(self.proj_names)):
+            elif not re.search(r'[a-zA-Z]', line) and line.strip() and len(line.strip().split()) - 2 == len(self.proj_names):
                 # data line, as there is only numerical values, not empty line, and expected number of
                 # columns (in case of LORBIT >= 12)
+                # DEBUGGING for corrupted PROCARs: (Uncomment these lines and _last_ion definition above)
+                # ion_no = int(line.strip().split()[0])
+                # if ion_no not in [_last_ion+1, 1]:
+                #     print(ion_no, _last_ion, line)
+                # _last_ion = ion_no
                 proj_data.append([float(token) for token in line.strip().split()[1:-1]])
 
             elif line.startswith('band'):
